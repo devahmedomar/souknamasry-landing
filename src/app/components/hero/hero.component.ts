878b9e577da1d4a1354c54109bc324221bd45cca
RehamReda216 <rehamreda216@gmail.com>
2025-04-22 09:05:59 +0200

    creating hero section

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  
 })
 export class HeroComponent {
+  @ViewChild('svgElement') svgRef!: ElementRef<SVGElement>;
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.svgRef) {
+        this.svgRef.nativeElement.classList.add('animated');
+      }
+    }, 1000);
+  }
+  
 }
