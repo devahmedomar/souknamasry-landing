59339286a34aec8b8255641ec208f0a37036706d
RehamReda216 <rehamreda216@gmail.com>
2025-05-04 20:43:21 +0300

    creating breadcrumb

--- src/app/shared/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumb/breadcrumb.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, input, OnInit, signal } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 @Component({
   selector: 'app-breadcrumb',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css'
 })
-export class BreadcrumbComponent {
-
+export class BreadcrumbComponent implements OnInit {
+  breadcrumbHeading= input<string>('');
+  items= signal<MenuItem[]>([]);
+  
+  ngOnInit() {
+      this.items.set([
+          { label: 'Home', routerLink: '/'},
+          { label: this.breadcrumbHeading()},
+      ]);
+  }
 }
