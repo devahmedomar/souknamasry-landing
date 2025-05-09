ed402c9864c1afa81983ac25cce9365874e086a1
Ahmed-Muhammad22 <ahmed.khater22698@gmail.com>
2025-05-10 00:41:27 +0300

    download ngx translate

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
-import { Component, NgModule, ViewChildren } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { FragmentActiveDirective } from '../directives/fragment-active.directive';
-import { CommonModule } from '@angular/common';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { MyTranslateService } from '../../services/myTranslate/my-translate.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, FragmentActiveDirective, CommonModule, RouterModule, RouterLinkActive],
+  imports: [RouterLink, FragmentActiveDirective, CommonModule, RouterModule, RouterLinkActive, TranslatePipe],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
- 
+  private readonly translateService = inject(TranslateService);
+  private readonly myTranslateService = inject(MyTranslateService)
+  changeLanguage(lang: string): void {
+    this.myTranslateService.changeLanguage(lang);
+  }
+
+  currentLang(lang:string): boolean {
+    return this.translateService.currentLang === lang ;
+  }
+
 }
 
