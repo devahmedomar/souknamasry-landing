b6751b6cd2f7cd60bc9dcd90e0da64fe26cc3192
shahd12121 <zazaza909090.zaza9z0a@gmail.com>
2025-05-14 20:52:52 +0300

    add translation by ts

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,20 +1,85 @@
-import { AfterViewInit, Component,  Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser, CommonModule } from '@angular/common'
+import { Component, AfterViewInit, OnInit, inject, DestroyRef, Inject, PLATFORM_ID, signal,} from '@angular/core';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import AOS from 'aos';
-import { TranslateModule} from '@ngx-translate/core';
-
 
 @Component({
   selector: 'app-services',
   standalone: true,
   imports: [CommonModule, TranslateModule],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrl: './services.component.css',
 })
-
-export class ServicesComponent implements AfterViewInit {
-  // Injects the PLATFORM_ID token to determine whether the code is running on the browser or server
+export class ServicesComponent implements AfterViewInit, OnInit {
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  // Inject services
+  private translate = inject(TranslateService);
+  private destroyRef = inject(DestroyRef);
+
+  // Signal to store translated services
+  services = signal<{ title: string; description: string; image: string }[]>([]);
+
+  ngOnInit(): void {
+    const lang$ = this.translate.onLangChange.pipe(takeUntilDestroyed(this.destroyRef));
+
+    const updateServices = () => {
+      this.translate
+        .get([
+          'servicesSection.cards.0.title',
+          'servicesSection.cards.0.description',
+          'servicesSection.cards.1.title',
+          'servicesSection.cards.1.description',
+          'servicesSection.cards.2.title',
+          'servicesSection.cards.2.description',
+          'servicesSection.cards.3.title',
+          'servicesSection.cards.3.description',
+          'servicesSection.cards.4.title',
+          'servicesSection.cards.4.description',
+          'servicesSection.cards.5.title',
+          'servicesSection.cards.5.description',
+        ])
+        .subscribe((translations) => {
+          this.services.set([
+            {
+              title: translations['servicesSection.cards.0.title'],
+              description: translations['servicesSection.cards.0.description'],
+              image: 'assets/images/ser1.jpg',
+            },
+            {
+              title: translations['servicesSection.cards.1.title'],
+              description: translations['servicesSection.cards.1.description'],
+              image: 'assets/images/ser2.avif',
+            },
+            {
+              title: translations['servicesSection.cards.2.title'],
+              description: translations['servicesSection.cards.2.description'],
+              image: 'assets/images/ser3.avif',
+            },
+            {
+              title: translations['servicesSection.cards.3.title'],
+              description: translations['servicesSection.cards.3.description'],
+              image: 'assets/images/ser4.avif',
+            },
+            {
+              title: translations['servicesSection.cards.4.title'],
+              description: translations['servicesSection.cards.4.description'],
+              image: 'assets/images/ser5.jpg',
+            },
+            {
+              title: translations['servicesSection.cards.5.title'],
+              description: translations['servicesSection.cards.5.description'],
+              image: 'assets/images/ser6.jpg',
+            },
+          ]);
+        });
+    };
+
+    updateServices();       // Initial load
+    lang$.subscribe(updateServices);  // Update on language change
+  }
+
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       setTimeout(() => {
@@ -22,39 +87,4 @@ export class ServicesComponent implements AfterViewInit {
       });
     }
   }
-  
-  services = [
-    {
-      title: 'servicesSection.cards.0.title',
-      description: 'servicesSection.cards.0.description',
-      image: 'assets/images/ser1.jpg'
-    },
-    {
-      title: 'servicesSection.cards.1.title',
-      description: 'servicesSection.cards.1.description',
-      image: 'assets/images/ser2.avif'
-    },
-    {
-      title: 'servicesSection.cards.2.title',
-      description: 'servicesSection.cards.2.description',
-      image: 'assets/images/ser3.avif'
-    },
-    {
-      title: 'servicesSection.cards.3.title',
-      description: 'servicesSection.cards.3.description',
-      image: 'assets/images/ser4.avif'
-    },
-    {
-      title: 'servicesSection.cards.4.title',
-      description: 'servicesSection.cards.4.description',
-      image: 'assets/images/ser5.jpg'
-    },
-    {
-      title: 'servicesSection.cards.5.title',
-      description: 'servicesSection.cards.5.description',
-      image: 'assets/images/ser6.jpg'
-    }
-  ];
-  
-
 }
