4e223aa05669961eba970ae97c68b7092fa16d72
RehamReda216 <rehamreda216@gmail.com>
2025-05-18 02:41:05 +0300

    create contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb.component';
+import { CardModule } from 'primeng/card';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbComponent,CardModule,TranslatePipe],  
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
