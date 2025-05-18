36f99193437a046204fd18683723fc706cface2b
RehamReda216 <rehamreda216@gmail.com>
2025-05-18 20:26:51 +0300

    create contact form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,22 +1,23 @@
-import { AfterViewInit, Component, Inject, PLATFORM_ID } from '@angular/core';
+import { AfterViewInit, Component, inject, Inject, PLATFORM_ID } from '@angular/core';
 import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb.component';
 import { CardModule } from 'primeng/card';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { isPlatformBrowser } from '@angular/common';
 import AOS from 'aos';
 import { contactData } from './contact-data';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [BreadcrumbComponent,CardModule,TranslatePipe],  
+  imports: [BreadcrumbComponent,CardModule,TranslatePipe,ButtonModule],  
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent implements AfterViewInit{
   contact_data = contactData
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  translate=inject(TranslateService)
+  private platformId = inject(PLATFORM_ID)
   
   ngAfterViewInit(): void {
       if (isPlatformBrowser(this.platformId)) {
