e94c2f6bf0552bd29a939455786047eff89aa81d
Ahmed-Muhammad22 <ahmed.khater22698@gmail.com>
2025-05-20 09:35:44 +0300

    put logo and download ngx spinner

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { FragmentActiveDirective } from '../directives/fragment-active.directive';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { MyTranslateService } from '../../services/myTranslate/my-translate.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-navbar',
@@ -15,6 +16,7 @@ import { MyTranslateService } from '../../services/myTranslate/my-translate.serv
     RouterModule,
     RouterLinkActive,
     TranslatePipe,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
@@ -22,18 +24,23 @@ import { MyTranslateService } from '../../services/myTranslate/my-translate.serv
 export class NavbarComponent implements OnInit {
   translateService = inject(TranslateService);
   myTranslateService = inject(MyTranslateService);
+  ngxSpinnerService = inject(NgxSpinnerService);
 
   currentLanguage: string = 'ar'; // default language
-
+  isLoading: boolean = true;
   ngOnInit(): void {
+    this.isLoading = true;
+    // this.ngxSpinnerService.show();
     // Subscribe to language change
     this.translateService.onLangChange.subscribe((event) => {
       this.currentLanguage = event.lang;
+      this.stopLoading();
     });
 
     // fallback in case language is already set
     if (this.translateService.currentLang) {
       this.currentLanguage = this.translateService.currentLang;
+      this.stopLoading();
     }
   }
 
@@ -45,5 +52,11 @@ export class NavbarComponent implements OnInit {
   changeLanguage(lang: string): void {
     this.myTranslateService.changeLanguage(lang);
     this.currentLanguage = lang;
+  }
+  private stopLoading(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+      // this.ngxSpinnerService.hide();
+    }, 500); // تأخير بسيط لضمان تهيئة الترجمة
   }
 }
